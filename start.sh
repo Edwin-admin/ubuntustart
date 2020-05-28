947dc0653247487b858fd10f5768c5854807fe2b
Edwin-admin <65607830+Edwin-admin@users.noreply.github.com>
2020-05-28 19:23:11 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,6 +2,14 @@
 sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt-get autoclean
-sudo apt-get install -y qemu qemu-utils qemu-kvm uml-utilities virt-manager virtinst dmg2img git ovmf libguestfs-tools libvirt-doc libvirt-daemon-system bridge-utils 
+sudo apt-get install -y dmg2img git ovmf
+
+# qemu KVM / virt-manager
+sudo apt-get install -y qemu qemu-utils qemu-kvm 
+sudo apt-get install -y uml-utilities virt-manager virtinst 
+sudo apt-get install -y libguestfs-tools libvirt-doc libvirt-daemon-system bridge-utils 
+
+# this is virtual box. 
+# sudo apt-get install -y virtualbox virtualbox-dkms virtualbox-ext-pack virtualbox-guest-additions-iso virtualbox-guest-utils virtualbox-qt
 sudo reboot now
 
