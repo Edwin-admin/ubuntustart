0b1a97b8ba51b84ff07afde1581b285c1bf92c2c
Edwin-admin <65607830+Edwin-admin@users.noreply.github.com>
2020-06-04 20:45:45 +0200

    Update stap2.sh

--- stap2.sh
+++ stap2.sh
@@ -3,4 +3,11 @@ cd ~/Documents/
 sudo git clone https://github.com/foxlet/macOS-Simple-KVM.git
 cd ~/Documents/macOS-Simple-KVM/
 sudo ./jumpstart.sh --mojave
-
+sudo qemu-img create -f qcow2 macOS.qcow2 80G
+echo "insert into ./basic.sh:"
+echo "-drive id=SystemDisk,if=none,file=macOS.qcow2 \"
+echo "-device ide-hd,bus=sata.4,drive=SystemDisk \"
+echo ""
+echo "start basic.sh:"
+echo "sudo ./basic.sh"
+sudo gedit basic.sh
